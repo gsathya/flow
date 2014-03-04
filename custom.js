60b652685a7510bc07b1d9d09a592b9f47fd9784
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-03-04 14:15:07 -0500

    add some lines

--- custom.js
+++ custom.js
@@ -6,3 +6,19 @@ L.tileLayer(osmUrl, {
     attribution: osmAttr,
     maxZoom: 18
 }).addTo(map);
+
+// create a red polyline from an arrays of LatLng points
+var pointA = new L.LatLng(28.635308, 77.22496);
+var pointB = new L.LatLng(28.984461, 77.70641);
+var pointC = new L.LatLng(28.984461, 77.90641);
+var pointList = [pointA, pointB, pointC];
+
+var polyline = new L.Polyline(pointList, {
+    color: 'red',
+    weight: 3,
+    opacity: 0.5,
+    smoothFactor: 1
+}).addTo(map);
+
+// zoom the map to the polyline
+map.fitBounds(polyline.getBounds());
