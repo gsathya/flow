c8bf9635fa8ad69c1788ea17f1fe393502248f72
msp8955 <msp8955@gmail.com>
2014-04-21 13:06:26 -0400

    modified

--- frontend/js/flow_monthly.js
+++ frontend/js/flow_monthly.js
@@ -19,7 +19,7 @@ function loadMap()
             var lat = obj.srclat;
             var lng = obj.srclng;
             var marker = L.marker([lat, lng]);
-            marker.bindPopup("<b>" + idx + "</b><br />");
+            marker.bindPopup("<b>" + idx + "</b><br />Double Click to View Route");
 	    (function(idx, lat, lng){ 
 		marker.on("dblclick", function() { selectSource(idx, lat, lng); });
 	    })(idx, lat, lng);
@@ -34,8 +34,8 @@ function loadMap()
 function selectSource(srcip, lat, lng)
 {
     markers.clearLayers();
-    var marker = L.marker([lat, lng]);
-    marker.bindPopup("<b>" + srcip + "</b><br />").openPopup();
+    var marker = L.marker([lat, lng]).addTo(map);
+    marker.bindPopup("<b>Source Location</b><br />" + srcip).openPopup();
     markers.addLayer(marker);
     var oReq = new XMLHttpRequest();
     oReq.onload = reqListener;
@@ -46,6 +46,9 @@ function selectSource(srcip, lat, lng)
             var dstLng = obj.dstLng;
 	    var marker = L.marker([dstLat, dstLng]);
 	    marker.bindPopup("<b>" + idx + "</b><br />");
+           (function(srcip, idx){
+            marker.on("dblclick", function() { mapSingleSrcDst(srcip, idx); });
+            })(srcip, idx);
 	    markers.addLayer(marker);
             var hopCount = obj.hopCount;
 	    var pathColor = get_random_color();
@@ -98,7 +101,10 @@ function searchByMac()
            var dstLng = obj.dstLng;
            var marker = L.marker([dstLat, dstLng]).addTo(map);
            marker.bindPopup("<b>" + idx + "</b><br />");
-           //markers.addLayer(marker);
+               (function(srcip, idx){
+                marker.on("dblclick", function() { mapSingleSrcDst(srcip, idx); });
+                })(srcip, idx);
+           markers.addLayer(marker);
            var hopCount = obj.hopCount;
            var pathColor = get_random_color();
            for(var i=0; i<hopCount; i++){
@@ -117,6 +123,47 @@ function searchByMac()
         });
     }
     var params = "mac=" + mac;
+    oReq.open("get", "/monthlystats?" + params, true);
+    oReq.send();
+}
+
+function mapSingleSrcDst(srcip, dstip){
+    
+    markers.clearLayers();
+    var oReq = new XMLHttpRequest();
+    oReq.onload = reqListener;
+    function reqListener(){
+        var json = JSON.parse(this.responseText);
+        var srcip = json.srcip;
+        var srcLat = json.srcLat;
+        var srcLng = json.srcLng;
+        var srcMarker = L.marker([srcLat, srcLng]).addTo(map);
+        srcMarker.bindPopup("<b>Source Location</b><br />srcIP: " + srcip).openPopup();
+        $.each(json, function(idx, obj){
+               
+               var dstLat = obj.dstLat;
+               var dstLng = obj.dstLng;
+               var marker = L.marker([dstLat, dstLng]).addTo(map);
+               marker.bindPopup("<b>" + idx + "</b><br />");
+               //markers.addLayer(marker);
+               var hopCount = obj.hopCount;
+               var pathColor = get_random_color();
+               for(var i=0; i<hopCount; i++){
+               var pointA = new L.LatLng(obj.vertex_ip1_lat[i], obj.vertex_ip1_lng[i]);
+               var pointB = new L.LatLng(obj.vertex_ip2_lat[i], obj.vertex_ip2_lng[i]);
+               var points = [pointA, pointB];
+               var polyline = new L.Polyline(points, {
+                                             color: pathColor,
+                                             weight: 3,
+                                             opacity: 0.5,
+                                             smoothFactor: 1
+                                             });
+               polyline.bindPopup("<b>Info</b><br />Average RTT: " + obj.avg_rtt[i] + "<br />Persistence: " + obj.persistence[i] + "<br />Prevalence: " + obj.prevalence[i]);
+               markers.addLayer(polyline);
+               }
+               });
+    }
+    var params = "srcip=" + srcip + "&dstip=" + dstip;
     oReq.open("get", "/monthlystats?" + params, true);
     oReq.send();
 }
